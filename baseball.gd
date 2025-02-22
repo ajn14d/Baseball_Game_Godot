45bfc87cbe9af703fbfffa5302221e6385ca400d
nash <nash7151@yahoo.com>
2025-02-21 17:01:11 -0700

    new field

--- baseball.gd
+++ baseball.gd
@@ -19,9 +19,9 @@ func _process(delta: float) -> void:
 
 # Function to throw a fastball straight down
 func pitch_fastball():
-	ball_speed = randi_range(300, 600)
+	ball_speed = randi_range(800, 1200)
 	# Apply a downward impulse to the ball
-	linear_velocity = Vector2(randi_range(-30, 30), ball_speed)  # Apply a random Y velocity
+	linear_velocity = Vector2(0, ball_speed)  # Apply a random Y velocity
 
 	# Optionally, you can use `apply_impulse()` for more realistic physics
 	# apply_impulse(Vector2.ZERO, Vector2(0, fastball_speed))  # Impulse to go straight down
@@ -29,7 +29,7 @@ func pitch_fastball():
 func _on_ball_area_area_entered(area: Area2D) -> void:
 	if area.is_in_group("Bat") and not ball_hit:
 		# Get the bat's angle in degrees
-		var bat_angle = batter.bat_pivot.rotation_degrees - 67.5
+		var bat_angle = batter.bat_pivot.rotation_degrees - 45
 		
 		# Convert the angle to radians (since Godot's trig functions use radians)
 		var angle_in_radians = deg_to_rad(bat_angle)
